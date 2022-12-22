0ad3421f416893a8004a8c924481fec4786f3ce0
Paweł Marszał <marszal.pawel@yahoo.com>
2022-12-22 21:33:06 +0100

    Accept connection from clients
    Server thread will now accept the client connections.
    Until implemented, the user session will be terminated
    immediately afterwards.

--- chess-server/src/main/java/com/marshall/chessgame/server/Server.java
+++ chess-server/src/main/java/com/marshall/chessgame/server/Server.java
@@ -1,21 +1,34 @@
 package com.marshall.chessgame.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    public static void main(String[] args) {
+    private static ServerSocket serverSocket;
+
+    public static void main(String[] args) throws IOException {
         Runtime.getRuntime().addShutdownHook(new CleanUpHook());
         initializeServer();
         acceptClientConnections();
     }
 
-    private static void initializeServer() {
+    private static void initializeServer() throws IOException {
         // TODO: initialize the server: collections of player-handlers, ServerSocket etc.
+        serverSocket = new ServerSocket(0);
     }
 
-    private static void acceptClientConnections() {
+    private static void acceptClientConnections() throws IOException {
         while (true) {
-            // TODO: accept incoming clients and startup their sessions
+            Socket socket = serverSocket.accept();
+            initiateClientSession(socket);
         }
+    }
+
+    private static void initiateClientSession(Socket socket) throws IOException {
+        // TODO: implement me :)
+        socket.close();
     }
 
     /**
@@ -25,6 +38,10 @@ public class Server {
         @Override
         public void run() {
             // TODO: implement me :)
+            try {
+                serverSocket.close();
+            } catch (IOException ignored) {
+            }
         }
     }
 }
